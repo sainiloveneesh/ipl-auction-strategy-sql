21058ce9fe006cd5e7644f8493ad844898097c21
sainiloveneesh <168617900+sainiloveneesh@users.noreply.github.com>
2025-12-28 10:54:00 +0530

    Add comments and preview queries in SQL script

--- sql/data_preparation.sql
+++ sql/data_preparation.sql
@@ -1,1 +1,12 @@
+-- IPL Auction Strategy
+-- Data Preparation & Exploration
 
+-- Preview matches table
+SELECT *
+FROM matches
+LIMIT 10;
+
+-- Preview deliveries table
+SELECT *
+FROM deliveries
+LIMIT 10;

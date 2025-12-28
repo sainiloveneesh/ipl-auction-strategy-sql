460cd07728a7b78fa4ab7cb3c991d81c31bfaf23
sainiloveneesh <168617900+sainiloveneesh@users.noreply.github.com>
2025-12-28 12:22:17 +0530

    Update aggressive_batters.sql

--- sql/aggressive_batters.sql
+++ sql/aggressive_batters.sql
@@ -1,9 +1,10 @@
 -- Batting Strike Rate Calculation
 -- Wides excluded from balls faced and runs
 
+
 WITH batter_stats AS (
     SELECT
-        batter,
+        batsman,
         COUNT(CASE WHEN extras_type != 'wides' THEN 1 END) AS balls_faced,
         SUM(
             CASE 
@@ -12,12 +13,12 @@ WITH batter_stats AS (
             END
         ) AS runs_scored
     FROM ipl_ball
-    GROUP BY batter
+    GROUP BY batsman
     HAVING COUNT(CASE WHEN extras_type != 'wides' THEN 1 END) >= 500
 )
 
 SELECT
-    batter,
+    batsman,
     balls_faced,
     runs_scored,
     ROUND((runs_scored * 100.0) / balls_faced, 2) AS strike_rate
